5c4d7de1a91431c93bb428e10fc562c6de204c5d
Shafihu Mustapha <shafihumustapha0101@gmail.com>
2024-05-19 07:08:46 +0000

    Installed axios

--- src/app/page.js
+++ src/app/page.js
@@ -1,7 +1,12 @@
+"use client";
+
 import Image from "next/image";
+import axios from "axios";
+import { useState } from "react";
+import { BsSearch } from "react-icons/bi";
 
 export default function Home() {
-  const url = `https://api.openweathermap.org/data/2.5/weather?q={city name}&appid={API key}`;
+  const url = `https://api.openweathermap.org/data/2.5/weather?q=dubai&appid=${process.env.NEXT_PUBLIC_WEATHER_KEY}`;
 
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24"></main>
