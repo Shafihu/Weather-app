0b591b739d9be9a59a67b23cf4b18bc381f19493
Shafihu Mustapha <shafihumustapha0101@gmail.com>
2024-05-19 07:17:09 +0000

    Fetching data with axios

--- src/app/page.js
+++ src/app/page.js
@@ -6,9 +6,26 @@ import { useState } from "react";
 import { BsSearch } from "react-icons/bi";
 
 export default function Home() {
-  const url = `https://api.openweathermap.org/data/2.5/weather?q=dubai&appid=${process.env.NEXT_PUBLIC_WEATHER_KEY}`;
+  const [city, setCity] = useState("");
+  const [weather, setWeather] = useState({});
+  const [loadings, setLoading] = useState(false);
+
+  const url = `https://api.openweathermap.org/data/2.5/weather?q=dubai&units=imperial&appid=${process.env.NEXT_PUBLIC_WEATHER_KEY}`;
+
+  const fetchWeather = (e) => {
+    e.preventDefault();
+    setLoading(true);
+    axios.get(url).then((response) => {
+      setWeather(response.data);
+      console.log(response.data);
+      setLoading(false);
+    });
+    setCity("");
+  };
 
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24"></main>
+    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      <button onClick={fetchWeather}>Fetch data</button>
+    </main>
   );
 }
