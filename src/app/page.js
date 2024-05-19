8bf670e63a4a5fec64b3e3396aec1a04daa6848b
Shafihu Mustapha <shafihumustapha0101@gmail.com>
2024-05-19 07:42:17 +0000

    Added an overlay

--- src/app/page.js
+++ src/app/page.js
@@ -24,8 +24,14 @@ export default function Home() {
   };
 
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <Image src="/bg.jpg" layout="fill" alt="Background pic" />
-    </main>
+    <>
+      <div className="absolute top-0 left-0 right-0 bottom-0 bg-black/40 z-[10]"></div>
+      <Image
+        src="/bg.jpg"
+        layout="fill"
+        alt="Background pic"
+        className="object-cover"
+      />
+    </>
   );
 }
